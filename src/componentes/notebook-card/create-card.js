f48df4d2c9766b08e1ec0d5e9689b47d91622c6c
Lucas Badico <lucasgomesdesantana@gmail.com>
2025-05-18 16:06:44 -0300

    update notebook card

--- src/componentes/notebook-card/create-card.js
+++ src/componentes/notebook-card/create-card.js
@@ -1,5 +1,5 @@
 // Função de clonagem igual à usada na página real    
-function createCard(document, wrapper, data) {
+function createNotebookCard(document, wrapper, data) {
     const t = wrapper.querySelector('#card-template').content;
     const clone = document.importNode(t, true);
     clone.querySelector('.card-title').textContent = data.name;
