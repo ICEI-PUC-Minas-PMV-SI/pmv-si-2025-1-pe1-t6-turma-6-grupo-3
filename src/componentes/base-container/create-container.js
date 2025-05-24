b9c861ef79c95f2e054e51a9cd4cc4c8fe6ae075
Lucas Badico <lucasgomesdesantana@gmail.com>
2025-05-24 13:50:04 -0300

    add append to container

--- src/componentes/base-container/create-container.js
+++ src/componentes/base-container/create-container.js
@@ -1,9 +1,16 @@
- function createContainer(document, wrapper, { items = [] }) {
-    const tmpl = wrapper.querySelector('#container-template').content;
-    const clone = document.importNode(tmpl, true);
+function createContainer(document, wrapper, { id, items = [] }) {
+  const tmpl = wrapper.querySelector('#container-template').content;
+  const clone = document.importNode(tmpl, true);
 
-    const slot = clone.querySelector('[data-slot="items-slot"]');
-    items.forEach(el => slot.appendChild(el));
+  clone.firstElementChild.dataset.containerId = id;
+  const slot = clone.querySelector('[data-slot="items-slot"]');
+  items.forEach(el => slot.appendChild(el));
 
-    return clone;
-  }
+  return clone;
+}
+
+function appendToContainerSlot(document, id, item) {
+  const containerEl = document.querySelector(`[data-container-id="${id}"]`);
+  console.log("container", containerEl);
+  containerEl.appendChild(item);
+}
