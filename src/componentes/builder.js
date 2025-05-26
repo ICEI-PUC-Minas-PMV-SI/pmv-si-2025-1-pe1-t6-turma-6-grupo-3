3d7b80de73a6b3c992decc6c53462d62bbf5a444
Lucas Badico <lucasgomesdesantana@gmail.com>
2025-05-26 08:24:40 -0300

    add components creator

---
+++ src/componentes/builder.js
@@ -0,0 +1,51 @@
+const map = {
+    "base-container": [createContainer],
+    "base-header": [createHeader],
+    "base-layout": [createLayout],
+    "base-modal": [createModal, createModalButton, createModalField, createModalTextarea],
+    "content-card": [createContentCard],
+    "icons-selector": [createIconSelector],
+    // "login-form": [createLoginForm], // not used on pages after login
+    "notebook-card": [createNotebookCard],
+    "tags-selector": [createTagInput],
+};
+
+function buildComponentsCreator(
+    document, // DOM
+    basepath, // "../"
+    components,
+) {
+    let choosed_list
+    if (components === "*") {
+        choosed_list = Object.keys(map);
+    } else {
+        choosed_list = components;
+    }
+
+    const fetch_address = choosed_list.map(cp => [cp, `${basepath}/componentes/${cp}/${cp.split("-")[1]}.html`]);
+    const promises = fetch_address.map(([cp, add]) => 
+        fetch(add).then(res => res.text())
+        .then(html => {
+            console.log("html", html)
+            const wrapper = document.createElement('div');
+            wrapper.innerHTML = html;
+            return wrapper;
+        }).then(wrapper => 
+            map[cp].map(
+                fn => [fn.name,  (arg) => fn(document, wrapper, arg)]
+            )
+        )
+    );
+
+    return Promise.all(promises) 
+        .then(mtz => mtz.reduce(
+            (acc, list) => [...acc, ...list],[])
+        )
+        .then(list => list.reduce(
+                (acc, nameFunc) => ({
+                ...acc,
+                [nameFunc[0]]: nameFunc[1]
+            }), {})
+        )
+       
+}
